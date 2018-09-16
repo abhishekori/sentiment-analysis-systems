d39be792c0c2a5f30ecb146394160b89ef3b2e1d
abhishekori <abhikori1994@gmail.com>
2018-09-16 19:08:39 +0530

    read the positive and negetive  movie review and store it in a list

--- rulebased-sentiment-analysis.py
+++ rulebased-sentiment-analysis.py
@@ -3,4 +3,13 @@ from nltk.sentiment import vader
 
 sia = vader.SentimentIntensityAnalyzer()
 sia.polarity_scores("this place is disgusting")
-print(sia['compoud'])
+posFilePath='./rt-polaritydata/rt-polaritydata/rt-polarity.pos'
+negFilePath='./rt-polaritydata/rt-polaritydata/rt-polarity.neg'
+#print(sia['compoud'])/home/abhishek/ML/sentiment analysis/rt-polaritydata/rt-polaritydata
+
+with open(posFilePath, encoding="latin-1") as f:
+    posData=f.readlines()
+    
+with open(negFilePath,encoding="latin-1") as f:
+    negData=f.readlines()
+    
