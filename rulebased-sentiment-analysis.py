665722cb9824d667a43055b601cad090495bc06b
abhishekori <abhikori1994@gmail.com>
2018-10-10 02:44:04 +0530

    added dignostices function to test different classification functions

--- rulebased-sentiment-analysis.py
+++ rulebased-sentiment-analysis.py
@@ -13,3 +13,36 @@ with open(posFilePath, encoding="latin-1") as f:
 with open(negFilePath,encoding="latin-1") as f:
     negData=f.readlines()
     
+    
+    
+    
+def vaderSentiment(review):
+    return sia.polarity_scores(review)['compound']
+
+def getReviewSenti(sentimentCalc):
+    negReviews=[sentimentCalc(negReview) for negReview in negData]
+    posReviews=[sentimentCalc(posReview) for posReview in posData]
+    
+    return {'res-on-pos':posReviews,'res-on-neg':negReviews}
+
+def runDiagnostics(reviewResults):
+    posReviews=reviewResults['res-on-pos']
+    negReviwes=reviewResults['res-on-neg']
+    corctPosReviw=float(sum(x>0 for x in posReviews))
+    corctNegReviw=float(sum(x<0 for x in negReviwes))
+    perctTruePos=corctPosReviw/len(posReviews)
+    perctTrueNeg=corctNegReviw/len(negReviwes)
+    totalAccurate = corctPosReviw + corctNegReviw
+    total = len(posReviews) + len(negReviwes)
+    print("Accuracy of positive reviews="+"%.2f"%(perctTruePos*100)+"%")
+    print("Accuracy of negetive reviews="+"%.2f"%(perctTrueNeg*100)+"%")
+    print("Overall accuracy="+"%.2f"%(totalAccurate*100/total)+"%")
+    
+runDiagnostics(getReviewSenti(vaderSentiment))
+    
+    
+
+
+
+
+    
