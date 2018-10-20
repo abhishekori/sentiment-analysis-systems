a071b9494420f661e1ba9bec4a652e258e70bc4d
abhishekori <abhikori1994@gmail.com>
2018-10-20 21:21:43 +0530

    added dignosis method to measure the accuracy

--- machine-learning-based-setiment-analysis.py
+++ machine-learning-based-setiment-analysis.py
@@ -65,7 +65,33 @@ def nbSentimentCalc(review):
     problemFeatures = extractFeatures(problemInstance)
     return trainedNBClassifier.classify(problemFeatures)
 
-nbSentimentCalc("what an bad good movie")
+#nbSentimentCalc("what an bad good movie")
+def getTestReviewSetiment(nbSentimentCalc):
+    testNegRes = [nbSentimentCalc(review) for review in testNegRev]
+    testPosRes = [nbSentimentCalc(review) for review in testPosRev]
+    
+    labelToNum = {'positive':1,'negative':-1}
+    numericNegRes = [labelToNum[x] for x in testNegRes]
+    numericPosRes = [labelToNum[x] for x in testPosRes]
+    return {'results-on-positive':numericPosRes,'results-on-negetive':numericNegRes}
+
+def runDiagnostics(reviewResult):
+    posReviewRes = reviewResult['results-on-positive']
+    negReviewRes = reviewResult['results-on-negetive']
+    
+    numTruePositive = sum(x>0 for x in posReviewRes)
+    numTreuNegetive = sum(x<0 for x in negReviewRes)
+    
+    pctTruePos = float(numTruePositive)/len(posReviewRes)
+    pctTrueNeg = float(numTreuNegetive)/len(negReviewRes)
+    totalAccurate = numTreuNegetive+numTruePositive
+    total = len(posReviewRes)+len(negReviewRes)
+    print("accuracy of +ve rev ="+"%.2f"%(pctTruePos*100)+"%")
+    print("Accuracy of -ve rev ="+"%.2f"%(pctTrueNeg*100)+"%")
+    
+runDiagnostics(getTestReviewSetiment(nbSentimentCalc))
+    
+
 
 
 
